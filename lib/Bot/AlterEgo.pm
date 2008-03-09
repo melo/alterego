a83de011ad33d138e8dc9833eeb82c346ce75817
Pedro Melo <melo@simplicidade.org>
2008-03-09 15:31:41 +0000

    Added support for configuration file
    All information required for starting up the bot will be read from this file.
    
    We support two files:
    
     * `cwd`/.alter_ego_cfg[.ext];
     * ~/.alter_ego_cfg[.ext].
    
    The bot will use the first one found.
    
    It scans extensions like `json` or `yaml` to decide the format to use.
    
    We also define some default values in the basic constructor.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Bot/AlterEgo.pm
+++ lib/Bot/AlterEgo.pm
@@ -1,3 +1,44 @@
 package Bot::AlterEgo;
 
+use strict;
+use warnings;
+use utf8;
+use Config::Any;
+use Net::XMPP2::IM::Connection;
+
+#############################
+# Constructor and bot startup
+
+sub new {
+  my ($class) = @_;
+  
+  my $cfg = $class->_load_config;
+  
+  # Default values
+  $cfg->{connection}{resource} ||= 'AlterEgo';
+  $cfg->{connection}{initial_presence} = undef; # Disable automatic initial presence
+
+  # ...
+}
+
+
+#######
+# Utils
+
+sub _load_config {
+  my ($class) = @_;
+  
+  my $found = Config::Any->load_stems({
+    stems   => [ ".alter_ego_cfg", "$ENV{HOME}/.alter_ego_cfg" ],
+    use_ext => 1,
+  });
+  die "Config file not found, " unless @$found;
+  
+  # right now we use only the first one, we could use all of them and
+  # merge the configs in a future version
+  my ($file, $config) = %{$found->[0]};
+  
+  return $config;
+}
+
 1;
